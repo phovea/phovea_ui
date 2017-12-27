7681f20ee8bf5fae7fc45b8edcf33c657657959b
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-12-27 18:53:47 +0100

    fix imports

--- src/layout/internal/dropper.ts
+++ src/layout/internal/dropper.ts
@@ -2,11 +2,10 @@
  * Created by Samuel Gratzl on 01.08.2017.
  */
 import {ALayoutContainer} from './ALayoutContainer';
-import {ILayoutContainer} from 'phovea_ui/src/layout';
 import TabbingLayoutContainer from './TabbingLayoutContainer';
 import SplitLayoutContainer from './SplitLayoutContainer';
 import {dropAble} from 'phovea_core/src/internal/dnd';
-import {EOrientation, IDropArea} from './interfaces';
+import {EOrientation, IDropArea, ILayoutContainer} from './interfaces';
 import {AParentLayoutContainer} from './AParentLayoutContainer';
 
 
