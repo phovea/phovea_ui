4749fb18aca3dbe7a11f287e6b0967952fe31d4d
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2019-11-05 13:46:30 +0100

    Use jQuery default import
    We need to add the `.default` to the jQuery import to specificially get the jQueryStatic (jQuery object). Otherwise it will result in an incompatible type error.

--- src/_lazyBootstrap.ts
+++ src/_lazyBootstrap.ts
@@ -3,6 +3,7 @@ import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 export default function loadBootstrap(): Promise<JQueryStatic> {
   return Promise.all([
     import('imports-loader?jQuery=jquery!bootstrap-sass/assets/javascripts/bootstrap.js'),
-    import('jquery')])
-    .then((args) => args[1]);
+    import('jquery')
+  ])
+  .then((args) => args[1].default);
 }
