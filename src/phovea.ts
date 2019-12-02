a524daf2244344cd961a4cc71406300edbbbf128
oltionchampari <oltion.champari@datavisyn.io>
2019-12-02 09:58:56 +0100

    improved typings

--- src/phovea.ts
+++ src/phovea.ts
@@ -1,11 +1,12 @@
 import {IRegistry, asResource} from 'phovea_core/src/plugin';
+import {EP_PHOVEA_CORE_LOCALE, ILocaleEPDesc} from 'phovea_core/src/extensions';
 
 //register all extensions in the registry following the given pattern
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return System.import('./src/extension_impl'); }, {});
-  registry.push('epPhoveaCoreLocale', 'phoveaUiLocaleEN', function () {
+  registry.push(EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
     return System.import('./assets/locales/en/phovea.json').then(asResource);
-  }, {
+  }, <ILocaleEPDesc>{
     ns: 'phovea',
   });
 }
