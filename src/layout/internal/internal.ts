03bab77638d7a494e41b6be010d58ef0241f2669
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 10:01:00 +0200

    add default exports

--- src/layout/internal/internal.ts
+++ src/layout/internal/internal.ts
@@ -1,10 +1,16 @@
 export * from './interfaces';
 export * from './ALayoutContainer';
+export { default as ALayoutContainer } from './ALayoutContainer';
 export * from './AParentLayoutContainer';
-export * from './RootLayoutContainer';
+export {default as AParentLayoutContainer} from './AParentLayoutContainer';
+export {default as RootLayoutContainer} from './RootLayoutContainer';
 export * from './ASequentialLayoutContainer';
-export * from './SplitLayoutContainer';
+export {default as ASequentialLayoutContainer} from './ASequentialLayoutContainer';
+export {default as SplitLayoutContainer} from './SplitLayoutContainer';
 export * from './TabbingLayoutContainer';
+export {default as TabbingLayoutContainer} from './TabbingLayoutContainer';
 export * from './ViewLayoutContainer';
+export {default as ViewLayoutContainer} from './ViewLayoutContainer';
 export * from './LineUpLayoutContainer';
+export {default as LineUpLayoutContainer} from './LineUpLayoutContainer';
 export * from './dropper';
