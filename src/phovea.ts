1d0a689fa080a364495f3ab1a56bc6be042a9c22
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-26 07:44:34 +0200

    remove System.import()

--- src/phovea.ts
+++ src/phovea.ts
@@ -3,9 +3,9 @@ import {LocaleExtensionPointDesc, ILocaleEPDesc} from 'phovea_core';
 
 //register all extensions in the registry following the given pattern
 export default function (registry: IRegistry) {
-  //registry.push('extension-type', 'extension-id', function() { return System.import('./src/extension_impl'); }, {});
+  //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
   registry.push(LocaleExtensionPointDesc.EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
-    return System.import('./assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
+    return import('./assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
   }, <ILocaleEPDesc>{
     ns: 'phovea',
   });
