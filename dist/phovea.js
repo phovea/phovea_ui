44961e3f4778de5e8e7dcdd50bf76a6d1723a31a
anita-steiner <k9908600@gmx.at>
2020-06-02 13:23:36 +0200

    change for phovea_core extension points

--- dist/phovea.js
+++ dist/phovea.js
@@ -1,9 +1,9 @@
 import { PluginRegistry } from 'phovea_core';
-import { LocaleExtensionPointDesc } from 'phovea_core';
+import { EP_PHOVEA_CORE_LOCALE } from 'phovea_core';
 //register all extensions in the registry following the given pattern
 export default function (registry) {
     //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
-    registry.push(LocaleExtensionPointDesc.EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
+    registry.push(EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
         return import('./assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
     }, {
         ns: 'phovea',
--- src/phovea.ts
+++ src/phovea.ts
@@ -1,10 +1,10 @@
 import {IRegistry, PluginRegistry} from 'phovea_core';
-import {LocaleExtensionPointDesc, ILocaleEPDesc} from 'phovea_core';
+import {EP_PHOVEA_CORE_LOCALE, ILocaleEPDesc} from 'phovea_core';
 
 //register all extensions in the registry following the given pattern
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
-  registry.push(LocaleExtensionPointDesc.EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
+  registry.push(EP_PHOVEA_CORE_LOCALE, 'phoveaUiLocaleEN', function () {
     return import('./assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
   }, <ILocaleEPDesc>{
     ns: 'phovea',
