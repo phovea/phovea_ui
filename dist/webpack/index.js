957b5f20752819951269a48a768b31f52e9ea713
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-07-22 08:52:06 +0200

    update source-map files

--- dist/webpack/index.js
+++ dist/webpack/index.js
@@ -1,1 +1,6 @@
+/**
+ * this package contains utilities for creating flexible layouts based on the builder pattern
+ */
+export * from './_bootstrap';
+export * from './_font-awesome';
 //# sourceMappingURL=index.js.map
