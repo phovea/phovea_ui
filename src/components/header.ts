a9c1acc43065a04e0a6cd0528253cfeb76f97380
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-04-07 15:38:20 +0200

    change order to fix flexbox setup for modals

--- src/components/header.ts
+++ src/components/header.ts
@@ -56,10 +56,10 @@ const getTemplate = () => {
   <div class="modal-dialog" role="document">
       <div class="modal-content">
           <div class="modal-header">
+              <h4 class="modal-title">${I18nextManager.getInstance().i18n.t('phovea:ui.about')}</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label="${I18nextManager.getInstance().i18n.t('phovea:ui.close')}">
                   <span aria-hidden="true">&times;</span>
               </button>
-              <h4 class="modal-title">${I18nextManager.getInstance().i18n.t('phovea:ui.about')}</h4>
           </div>
           <div class="modal-body" data-header="about">
               <div class="metaData">${I18nextManager.getInstance().i18n.t('phovea:ui.loading')}</div>
@@ -81,10 +81,10 @@ const getTemplate = () => {
   <div class="modal-dialog" role="document">
       <div class="modal-content">
           <div class="modal-header">
+              <h4 class="modal-title"> ${I18nextManager.getInstance().i18n.t('phovea:ui.reportBug')}</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label=" ${I18nextManager.getInstance().i18n.t('phovea:ui.close')}">
                   <span aria-hidden="true">&times;</span>
               </button>
-              <h4 class="modal-title"> ${I18nextManager.getInstance().i18n.t('phovea:ui.reportBug')}</h4>
           </div>
           <div class="modal-body" data-header="bug">
           ${I18nextManager.getInstance().i18n.t('phovea:ui.loading')}
@@ -98,10 +98,10 @@ const getTemplate = () => {
   <div class="modal-dialog" role="document">
       <div class="modal-content">
           <div class="modal-header">
+              <h4 class="modal-title"> ${I18nextManager.getInstance().i18n.t('phovea:ui.options')}</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label=" ${I18nextManager.getInstance().i18n.t('phovea:ui.close')}">
                   <span aria-hidden="true">&times;</span>
               </button>
-              <h4 class="modal-title"> ${I18nextManager.getInstance().i18n.t('phovea:ui.options')}</h4>
           </div>
           <div class="modal-body" data-header="options">
           ${I18nextManager.getInstance().i18n.t('phovea:ui.loading')}
