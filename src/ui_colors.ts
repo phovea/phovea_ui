8a885cdc00922e06dc9bce8b68715b41b6524e69
Daniela <daniela.moitzi@datavisyn.io>
2021-09-16 10:50:00 +0200

    add selection colors

---
+++ src/ui_colors.ts
@@ -0,0 +1,5 @@
+// selection / hover colors
+export const selection_color_light = '#ffcf76';
+export const selection_color_dark = '#e29609';
+export const selection_color_light_hovered = '#ffe8be';
+export const selection_color_dark_hovered = '#ffcf76';
